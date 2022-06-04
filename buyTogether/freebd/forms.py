559c7051662404fcd4a656b87ff46db93bdb7e4f
Jaehyun Kim <j9709h@naver.com>
2022-06-04 16:13:52 +0900

    김재현 파트

--- buyTogether/freebd/forms.py
+++ buyTogether/freebd/forms.py
@@ -6,7 +6,17 @@ class Freemodelform(forms.ModelForm):
 		model = Free
 		fields = ['title','body','photo']
 
+		widgets = {
+			'title' : forms.TextInput(
+				attrs={'class': 'form-control', 'style': 'width: 400px', 'placeholder': '제목을 입력하세요.'}
+			),
+			'body' : forms.TextInput(
+				attrs={'class': 'form-control', 'style': 'width: 400px', 'placeholder': '내용을 입력하세요.'}
+			),
+		}
+
 class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
 		fields = ['body']
+
