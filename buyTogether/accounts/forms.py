aa0ed10239e180e2df9c46c7503f5cbd0809a938
JuHyun Yoon <21720819@gmail.com>
2022-06-08 16:08:05 +0900

    ii

--- buyTogether/accounts/forms.py
+++ buyTogether/accounts/forms.py
@@ -4,9 +4,9 @@ from .models import User
 class Userform(forms.ModelForm):
 	class Meta:
 		model = User
-		fields = ['userID','password','nickname']
+		fields = ['userID','password','username']
 		widgets = {
-            'nickname': forms.TextInput(attrs={'class': 'form-control item', 'placeholder':'닉네임'}),
+            'username': forms.TextInput(attrs={'class': 'form-control item', 'placeholder':'닉네임'}),
             'userID': forms.EmailInput(attrs={'class': 'form-control item', 'placeholder':'이메일'}),
             'password' : forms.PasswordInput(attrs={'class': 'form-control item', 'placeholder':'비밀번호'}),
         }
